bc1db09f4c11d8eca67ba2baa564586a7dbf6dfb
pappudey <pappudey.coder@gmail.com>
2023-07-24 20:25:32 +0600

    server updated successfully

--- server/server.js
+++ server/server.js
@@ -14,11 +14,9 @@ app.disable('x-powered-by');
 
 
 const corsConfig = {
-    
     origin: 'https://account-authentication-system-x1ms.vercel.app',
-  methods: 'GET, POST, PUT, DELETE',
-  allowedHeaders: 'Content-Type, Authorization',
-  credentials: true,
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE']
 }
 app.use(cors(corsConfig))
 app.options("", cors(corsConfig))
