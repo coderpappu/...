79febedcc1a7684fc3c2d9ae85481387127ae0c5
pappudey <pappudey.coder@gmail.com>
2023-07-24 17:39:18 +0600

    server updated

--- server/server.js
+++ server/server.js
@@ -14,7 +14,7 @@ app.disable('x-powered-by');
 
 
 const corsConfig = {
-    origin: 'http://localhost:5173',
+    origin: 'https://account-authentication-system-x1ms.vercel.app',
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE']
 }
